838a062adb822cd502ef24f74140a5798676462b
Rika Umra <rikaumra@gmail.com>
2021-01-21 10:29:48 +0200

    modified src/main/java/com/mavenprolab/Welcome.java

--- src/main/java/com/mavenprolab/Welcome.java
+++ src/main/java/com/mavenprolab/Welcome.java
@@ -5,6 +5,8 @@ public class Welcome {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to MavenGITJenkins Lab");
+		System.out.println("Hello GITHOOK");
+		System.out.println("Hello GITHOOK");
 	}
 
 }
